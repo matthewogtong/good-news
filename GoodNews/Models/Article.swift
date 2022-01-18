f48e1f479cc3948aa2e6c89fb6e52a033997de48
matthewogtong <ogtongm@gmail.com>
2022-01-18 08:46:32 -0500

    Create new group models and new article struct model

---
+++ GoodNews/Models/Article.swift
@@ -0,0 +1,15 @@
+//
+//  Article.swift
+//  GoodNews
+//
+//  Created by Matthew Ogtong on 1/18/22.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    
+    let title: String
+    let descriiption: String
+    
+}
