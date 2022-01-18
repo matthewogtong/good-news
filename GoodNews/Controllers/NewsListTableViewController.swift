a33e17701fcc72c27691694a6abd1e699df290f7
matthewogtong <ogtongm@gmail.com>
2022-01-17 20:55:01 -0500

    Initialize viewDidLoad and setup methods to display a larger title

--- GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        setup()
+        
+    }
     
-    
+    private func setup() {
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
 }
