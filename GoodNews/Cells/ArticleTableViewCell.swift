f7f47a1bd329e9aedb21241870adf4bca7c99f66
matthewogtong <ogtongm@gmail.com>
2022-01-19 17:22:57 -0500

    Create new Cells group and class for Article Table View Cell along with connect class and IBOutlets

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Matthew Ogtong on 1/19/22.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
